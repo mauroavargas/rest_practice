066679c5be9f1317827eb5d1482813aac59525b7
Mauro Vargas <maurovargas@MacBook-Air-de-Mauro.local>
2020-02-19 21:26:36 -0500

    Configuración inicial proyecto

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 4000;
+
+// Endpoints ES6
+server.get('/', (req, res) => res.send('Hello Guys!'));
+
+// Endppoint JS Vanilla
+server.get('/prueba', function(req, res){
+    res.send('Hello Prueba!');
+});
+
+// Turn the server ON
+server.listen(PORT, () => console.log(`Server on port ${PORT}!`));
